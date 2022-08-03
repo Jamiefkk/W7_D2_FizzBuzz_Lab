f7608a702c45f193fd442e0f49fe2a77a9f1606e
Jamie <jamiefogartykk@gmail.com>
2022-08-03 11:34:37 +0100

    First Extensions

--- src/App.js
+++ src/App.js
@@ -6,19 +6,21 @@ function App() {
 
   const [number, setNumber] = useState(0);
   const [fizzBuzzAnswer, setFizzBuzzAnswer] = useState(0);
+  const [var1, setVar1] = useState(3);
+  const [var2, setVar2] = useState(5);
 
   useEffect(() => {
     console.log("use effect triggers")
-    if (number % 3 === 0 && number % 5 === 0) {
+    if (number % var1 === 0 && number % var2 === 0) {
       setFizzBuzzAnswer("fizzbuzz")
-    } else if (number % 3 === 0) {
+    } else if (number % var1 === 0) {
       setFizzBuzzAnswer("fizz")
-    } else if (number % 5 === 0) {
+    } else if (number % var2 === 0) {
       setFizzBuzzAnswer("buzz")
     } else {
       setFizzBuzzAnswer(number);
     }
-  }, [number]);
+  }, [number, var1, var2]);
 
   const handleInc = () => {
     setNumber(number + 1);
@@ -27,19 +29,15 @@ function App() {
   const handleDec = () => {
     setNumber(number - 1);
   };
-  
-  
-  // const fizzBuzzCheck = (number) => {
-  //   if (number % 3 === 0 && number % 5 === 0) {
-  //     setFizzBuzzAnswer("fizzbuzz")
-  //   } else if (number % 3 === 0) {
-  //     setFizzBuzzAnswer("fizz")
-  //   } else if (number % 5 === 0) {
-  //     setFizzBuzzAnswer("buzz")
-  //   } else {
-  //     setFizzBuzzAnswer(number);
-  // }};
 
+  const handleVar1Change = (event) => {
+    setVar1(event.target.value)
+  }
+  
+  const handleVar2Change = (event) => {
+    setVar2(event.target.value)
+  }
+  
 
   return (
     <>
@@ -47,6 +45,18 @@ function App() {
         <button value={number} onClick={handleInc}>+</button>
         <button value={number} onClick={handleDec}>-</button>
         <h2>Number is: {number}</h2>
+        <form>
+          <input 
+          type="number" 
+          placeholder="choose fizz number" 
+          value={var1}
+          onChange={handleVar1Change}/>
+          <input 
+          type="number" 
+          placeholder="choose buzz number" 
+          value={var2}
+          onChange={handleVar2Change}/>
+        </form>
         <h3>{fizzBuzzAnswer}</h3>
     </>
   );
