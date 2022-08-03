678fcb8bf616697986ef2f160db75c8a84180115
Jamie <jamiefogartykk@gmail.com>
2022-08-03 11:13:01 +0100

    MVP

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
+
 
 function App() {
+
+  const [number, setNumber] = useState(0);
+  const [fizzBuzzAnswer, setFizzBuzzAnswer] = useState(0);
+
+  useEffect(() => {
+    console.log("use effect triggers")
+    if (number % 3 === 0 && number % 5 === 0) {
+      setFizzBuzzAnswer("fizzbuzz")
+    } else if (number % 3 === 0) {
+      setFizzBuzzAnswer("fizz")
+    } else if (number % 5 === 0) {
+      setFizzBuzzAnswer("buzz")
+    } else {
+      setFizzBuzzAnswer(number);
+    }
+  }, [number]);
+
+  const handleInc = () => {
+    setNumber(number + 1);
+  };
+
+  const handleDec = () => {
+    setNumber(number - 1);
+  };
+  
+  
+  // const fizzBuzzCheck = (number) => {
+  //   if (number % 3 === 0 && number % 5 === 0) {
+  //     setFizzBuzzAnswer("fizzbuzz")
+  //   } else if (number % 3 === 0) {
+  //     setFizzBuzzAnswer("fizz")
+  //   } else if (number % 5 === 0) {
+  //     setFizzBuzzAnswer("buzz")
+  //   } else {
+  //     setFizzBuzzAnswer(number);
+  // }};
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <h1>FizzBuzz</h1>
+        <button value={number} onClick={handleInc}>+</button>
+        <button value={number} onClick={handleDec}>-</button>
+        <h2>Number is: {number}</h2>
+        <h3>{fizzBuzzAnswer}</h3>
+    </>
   );
 }
 
